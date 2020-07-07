a58db8a529d6c0ea96bf4c2fa39f374bc3dd21b0
Shlomi Fish <shlomif@shlomifish.org>
2020-07-07 17:06:36 +0300

    got dzil test --all to pass

--- Text-SortWords/lib/Text/SortWords.pm
+++ Text-SortWords/lib/Text/SortWords.pm
@@ -1,6 +1,27 @@
 package Text::SortWords;
 
+use 5.014;
 use strict;
 use warnings;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Text::SortWords - sort individual "words" in a text while preserving separators/delimiters .
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2
+
+=head2
+
+=cut
